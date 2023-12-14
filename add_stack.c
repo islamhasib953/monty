b1294bf43899b11a6d463da8fe55cf179f486f3d
islamhasib953 <islamhasib953@gmail.com>
2023-12-14 18:11:34 +0200

    Update

---
+++ add_stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * set_stack_mode - sets the stack mode
+ * @stack_head: stack head
+ * @line_number: line_number
+ * Return: no return
+ */
+void set_stack_mode(stack_t **stack_head, unsigned int line_number)
+{
+    (void)stack_head;
+    (void)line_number;
+    heads.check = 0;
+}
